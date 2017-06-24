79ce6bb4089680204b1d5f8443e9ba99c9fa446a
Candice <wuqiong910718@163.com>
2017-06-24 10:14:01 +0800

    tabbar的选择，刷新页面根据router path 选中tabber

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,26 +4,26 @@
       <router-view></router-view>
       <mt-tabbar v-model="selected" fixed>
 
-        <mt-tab-item id="home" >
+        <mt-tab-item id="/home" >
           <img slot="icon" src="">
           首页
         </mt-tab-item>
 
-        <mt-tab-item id="circle">
+        <mt-tab-item id="/circle">
           <img slot="icon" src="">
           圈子
         </mt-tab-item>
 
-        <mt-tab-item id="activity">
+        <mt-tab-item id="/activity">
           <img slot="icon" src="">
           活动
         </mt-tab-item>
 
-        <mt-tab-item id="message">
+        <mt-tab-item id="/message">
           <img slot="icon" src="">
           消息
         </mt-tab-item>
-        <mt-tab-item id="account">
+        <mt-tab-item id="/account">
           <img slot="icon" src="">
           我的
         </mt-tab-item>
@@ -43,6 +43,14 @@ export default {
       // 这里就可以通过 val 的值变更来确定当前选中的tab
       this.$router.push({'path': val})
     }
+  },
+  created () {
+    let currentRouter = this.$route.path
+    if (currentRouter === '/index') {
+      currentRouter = '/home'
+    }
+    this.selected = currentRouter
+    this.$router.push({'path': currentRouter})
   }
 }
 </script>
